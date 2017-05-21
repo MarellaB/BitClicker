b06e87bac49afbae1f401965ced2a0a6029392fc
Wesley Brown <brown.5776@osu.edu>
2017-05-20 21:01:58 -0400

    Add more items

--- index.js
+++ index.js
@@ -1,39 +1,4 @@
 /**
- * Manages items that the player can buy.
- * @type {Object}
- */
-const itemManager = {
-
-  /**
-   * All the items available for purchase.
-   * @type {Object}
-   */
-  ITEMS: [
-    {
-      name: 'potato',
-      price: 1
-    },
-    {
-      name: 'lemon',
-      price: 4
-    }
-  ],
-
-  /**
-   * Give the given player an item if it has enough money.
-   * @param {Number} itemId
-   */
-  buyItem(player, itemId) {
-    if (player.coins >= this.ITEMS[itemId].price) {
-      player.coins -= 1;
-      player.addItem(itemId);
-    } else {
-      console.error(`You can't buy a ${this.ITEMS[itemId].name}, ya dingus! You need $${this.ITEMS[itemId].price} and only have $${player.coins}!`);
-    }
-  }
-};
-
-/**
  * The player.
  * @type {Object}
  */
@@ -52,13 +17,31 @@ const player = {
   items: [
     {
       name: 'potato',
+      price: 1,
+      generates: 0.1,
       amount: 0
     },
     {
       name: 'lemon',
+      price: 4,
+      generates: 1,
+      amount: 0
+    },
+    {
+      name: 'computerStick',
+      price: 100,
+      generates: 2,
+      amount: 0
+    },
+    {
+      name: 'dualCoreProcessor',
+      price: 300,
+      generates: 4,
       amount: 0
     }
   ],
+
+  generatePerTick: 0,
 
   /**
    * The upgrades that the player has purchased.
@@ -68,6 +51,20 @@ const player = {
 
   addItem(itemId) {
     this.items[itemId].amount++;
+  },
+
+  /**
+   * Give the given player an item if it has enough money.
+   * @param {Number} itemId
+   */
+  buyItem(itemId) {
+    if (this.coins >= this.items[itemId].price) {
+      this.coins -= 1;
+      this.items[itemId].amount++;
+      this.generatePerTick += this.items[itemId].generates;
+    } else {
+      console.error(`You can't buy a ${this.items[itemId].name}, ya dingus! You need $${this.items[itemId].price} and only have $${player.coins}!`);
+    }
   }
 
 };
@@ -81,20 +78,31 @@ const game = {
   purchasePototoButton: null,
   purchaseLemonButton: null,
 
+  generateAmount: 0,
+  moneyCount: 0,
+
+  generatePerTick: 0,
+
   init() {
     this.purchasePotatoButton = document.getElementById('purchasePotato');
     this.purchaseLemonButton = document.getElementById('purchaseLemon');
+    this.purchaseComputerStickButton = document.getElementById('purchaseCompStick');
 
     this.purchasePotatoButton.addEventListener('click', this.buyPotato);
     this.purchaseLemonButton.addEventListener('click', this.buyLemon);
+    this.purchaseComputerStickButton.addEventListener('click', this.buyComputerStick);
   },
 
   buyPotato() {
-    itemManager.buyItem(player, 0);
+    player.buyItem(0);
   },
 
   buyLemon() {
-    itemManager.buyItem(player, 1);
+    player.buyItem(1);
+  },
+
+  buyComputerStick() {
+    player.buyItem(2);
   }
 
 };
